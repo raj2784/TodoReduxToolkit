602f142e6b66b7ada6621b5760e2d151bf7756f2
Raj Bhatia <raj2784@hotmail.com>
2024-01-25 08:31:34 +1030

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import AddTodo from "./components/AddTodo";
+import { Provider } from "react-redux";
+import Todos from "./components/Todos";
+import { store } from "./store";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Provider store={store}>
+        <div className="text-3xl text-center font-bold underline text-white mt-7">
+          <h2>React Redux Toolkit</h2>
+          <AddTodo />
+          <Todos />
+        </div>
+      </Provider>
+      ;
+    </>
   );
 }
-
 export default App;
